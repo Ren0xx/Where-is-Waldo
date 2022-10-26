5c70cc84a59b5cd3df8cd6b476d00c2f45a196fd
Adam Załęski <adamzaleski001@wp.pl>
2022-10-26 14:59:10 +0200

    Handling user login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import InputAdornment from "@mui/material/InputAdornment";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import KeyIcon from "@mui/icons-material/Key";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 import { app } from "../firebaseConfig";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
+    const [login, setLogin] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
     const auth = getAuth(app);
+
+    const handleLogin = (login: string, password: string) => {
+        signInWithEmailAndPassword(auth, login, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+            });
+    };
+
     return (
         <Container
             sx={{
@@ -19,14 +41,25 @@ const Login = () => {
                 alignItems: "center",
                 mb: 5,
                 height: "100vh",
+                backgroundColor: "#d4c9c3",
             }}>
-            <Box
+            <Stack
+                spacing={5}
+                direction='column'
                 component='form'
                 autoComplete='off'
-                sx={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+                sx={{
+                    backgroundColor: "#fff",
+                    padding: "3rem",
+                    width: "300px",
+                    height: "auto",
+                    borderRadius: "9px",
+                }}>
                 <TextField
                     label='Email'
                     id='outlined'
+                    required
+                    placeholder='Type your login'
                     InputProps={{
                         startAdornment: (
                             <InputAdornment position='start'>
@@ -34,10 +67,15 @@ const Login = () => {
                             </InputAdornment>
                         ),
                     }}
+                    onChange={(event: any) => {
+                        setLogin(event.target.value);
+                    }}
                 />
                 <TextField
                     id='outlined-password-input'
                     label='Password'
+                    required
+                    placeholder='Type your password'
                     type='password'
                     InputProps={{
                         startAdornment: (
@@ -46,8 +84,18 @@ const Login = () => {
                             </InputAdornment>
                         ),
                     }}
+                    onChange={(event: any) => {
+                        setPassword(event.target.value);
+                    }}
                 />
-            </Box>
+                <Button
+                    variant='contained'
+                    size='large'
+                    sx={{ borderRadius: "15px", backgroundColor: "#7dcce5" }}
+                    onClick={() => handleLogin(login, password)}>
+                    Login
+                </Button>
+            </Stack>
         </Container>
     );
 };
